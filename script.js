ec2e62215c4819a46a7d6586b225abf08ac406bd
pedrosa <pedrosa@outlook.com>
2025-04-24 10:32:53 -0300

    Atualizando código do botão no arquivo javascript

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-console.log("Easter egg")
-let alteraImg = document.querySelectorAll("img")
+console.log("Easter egg");
 
-alteraImg.addEventListener("mouseover", trocaCor)
+document.getElementById("botao").addEventListener("click", function () {
+    if (!document.getElementById("mensagem")) { 
+        const mensagem = document.createElement("p");
+        mensagem.id = "mensagem"; 
+        mensagem.textContent = "Oi!";
+        document.getElementById("container").appendChild(mensagem);
+    }
+});
+
 
